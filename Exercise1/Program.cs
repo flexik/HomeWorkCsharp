510df4ae3fe37a9ae0ea22184f7b936fd79cc67a
Artyome <flex785834@yandex.ru>
2023-01-30 22:52:12 +0300

    Сделал первые 2 задачи из ДЗ

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,17 +1,19 @@
-﻿// Задача 2 (№1)
+﻿// Задача 2 (№1) Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 Console.Clear();
 Console.WriteLine("Напишите первое число");
 int number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Напишите второе число");
 int number2 = int.Parse(Console.ReadLine());
 
-if (number1 == number2) {
-    Console.WriteLine($"Числа равны, запустите программу и введите числа заново");
-    return;
-}
-if (number2 < number1) {
+if (number2 < number1){
     Console.WriteLine($"Первое число: {number1} больше второго: {number2}");
 }
-if (number1 < number2) {
+if (number1 < number2){
     Console.WriteLine($"Второе число: {number2} больше первого: {number1}");
+}
+else
+{
+    Console.WriteLine($"Числа {number1} и {number2} равны, запустите программу и введите числа заново");
+    return;
 }
---
+++ Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4 (№2) Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Напишите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите третье число");
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1 > number2 & number1 > number3){
+    Console.WriteLine($"Максимальное число: {number1}");
+}
+if (number2 > number1 & number2 > number3){
+    Console.WriteLine($"Максимальное число: {number2}");
+}
+if (number3 > number1 & number3 > number2){
+    Console.WriteLine($"Максимальное число: {number3}");
+}
+else
+{
+    Console.WriteLine($"Вы ввели одинаковые числа - запустите программу и введите числа заново");
+    return;
+}
