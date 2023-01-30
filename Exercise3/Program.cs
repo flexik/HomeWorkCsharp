972b9081d08f9ae97d0a7f6de05bb5eb0413f42d
Artyome <flex785834@yandex.ru>
2023-01-30 23:15:37 +0300

    Решил 3 задачу

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6 (№3)Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Напишите натуральное число для проверки на чётность");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 0 || number < 0)
+{
+    Console.WriteLine("Вы ввели 0 или отрицательное число - запустите программу заново и повторите попытку");
+    return;
+}
+if (number % 2 == 0){
+    Console.WriteLine($"Число {number} является чётным");
+}
+if (number % 2 == 1){
+    Console.WriteLine($"Число {number} является нечётным");
+}
