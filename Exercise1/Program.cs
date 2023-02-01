3b146c8ebd5ec7ad874e1cb3ae8586e7ef4c4b35
Artyome <flex785834@yandex.ru>
2023-02-01 17:35:37 +0300

    Сделал все задания

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Задача 2 (№1) Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
 Console.Clear();
 Console.WriteLine("Напишите первое число");
 int number1 = int.Parse(Console.ReadLine());
@@ -9,7 +8,7 @@ int number2 = int.Parse(Console.ReadLine());
 if (number2 < number1){
     Console.WriteLine($"Первое число: {number1} больше второго: {number2}");
 }
-if (number1 < number2){
+else if (number1 < number2){
     Console.WriteLine($"Второе число: {number2} больше первого: {number1}");
 }
 else
--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8 (#4): Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Напишите число");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while(count <= N){
+    if (count % 2 == 0){
+        Console.WriteLine($"{count}");
+        count ++;
+    }
+    if (count % 2 == 1){
+        count ++;
+    }
+    else
+    {
+        Console.WriteLine("Введите натуральное число и повторите попытку");
+        break;
+    }
+}
