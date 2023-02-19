48579f6e9ba27c2747bc6b4ddd81410151a2f4f2
Artyome <flex785834@yandex.ru>
2023-02-20 01:13:53 +0300

    сделал ДЗ №6

---
+++ Exercise19(sem6)/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+
+Console.Clear();
+
+Console.WriteLine("Введите целые числа через пробел:");
+string elements = Console.ReadLine(); // получаем строку из элементов, которые ввёл юзер
+int[] arrayFromUser = GetArrayFromString(elements); // помещаем элементы в массив с помощью метода преобразования из строки в массив
+Console.WriteLine(String.Join(", ", FindPositiveElements(arrayFromUser))); // вывод результата метода, который определяет кол-во положительных чисел в массиве
+
+int[] GetArrayFromString(string stringArray) // метод преобразования из строки в массив
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = int.Parse(nums[i]);
+    }
+    return result;
+}
+
+int FindPositiveElements(int[] array)
+{ // метод, который считает кол-во положительных чисел в массиве
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
---
+++ Exercise20(sem6)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+
+Console.Clear();
+
+Console.WriteLine("Введите число-значение b1:"); // 2
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число-значение k1:"); // 5
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число-значение b2:"); // 4
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число-значение k2:"); // 9
+double k2 = double.Parse(Console.ReadLine());
+
+double[] IntersectionPointOfLines(double b1, double k1, double b2, double k2)  // метод определения пересечения прямых
+{ 
+    double pointX = (b2 - b1) / (k1 - k2); //   x = 2/-4 = -0,5
+    double pointY = k1 * pointX + b1; //   y = 5 * (-0,5) + 2 = -0,5
+    double[] result = new double[2] {pointX, pointY};
+    return result;
+}
+
+Console.WriteLine(String.Join("; ", IntersectionPointOfLines(b1, k1, b2, k2)));
